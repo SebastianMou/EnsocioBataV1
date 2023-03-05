b0250d9531bac2bede7274fa86f8d8afdc8fbeca
SebastianMou <Sebastian_Mou@protonmail.com>
2023-03-05 15:50:29 -0600

    make it better

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#w6h08m+s3-nkc(p+wzv@vwz(dhv6xm-hk_w!l80t+p%j5rd4q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -326,6 +326,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 
 MEDIA_URL = 'media/'
